b3f388e5869e559d89b73c96c1d47f30bef63c85
HubertMakowski <h.p.makowski@gmail.com>
2019-06-06 21:58:17 +0200

    fix bug with vh, vmax for phone keyboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
@@ -11,29 +11,40 @@ import PrevPathUpdater from 'utils/PrevPathUpdater';
 import { ProfilePage } from 'components/Pages/Welcome/ProfilePage';
 import { ListPage } from 'components/Pages/Welcome/ListPage';
 
-export const App = () => (
-	<Router>
-		<Route
-			render={({ location }) => {
-				return (
-					<TransitionGroup component={PrevPathUpdater}>
-						<CSSTransition
-							timeout={200}
-							classNames="page"
-							key={location.key}
-						>
-							<Switch location={location}>
-								<Route exact path={routes.WELCOME_EXPLORE} component={ExplorePage} />
-								<Route exact path={routes.WELCOME_COLLECT} component={CollectPage} />
-								<Route exact path={routes.WELCOME_JOIN} component={JoinPage} />
-							</Switch>
-						</CSSTransition>
-					</TransitionGroup>
-				);
-			}}
-		/>
-		<Route path={routes.MAP} component={MapPage}/>
-		<Route path={routes.WELCOME_PROFILE} component={ProfilePage}/>
-		<Route path={routes.WELCOME_LIST} component={ListPage}/>
-	</Router>
-);
+export class App extends Component {
+	componentDidMount() {
+		let viewHeight = window.outerHeight;
+		let viewWidth = window.outerWidth;
+		let viewport = document.querySelector('meta[name=viewport]');
+		viewport.setAttribute('content', `height=${viewHeight}, width=${viewWidth}, initial-scale=1.0`);
+	}
+
+	render() {
+		return (
+			<Router>
+				<Route
+					render={({ location }) => {
+						return (
+							<TransitionGroup component={PrevPathUpdater}>
+								<CSSTransition
+									timeout={200}
+									classNames="page"
+									key={location.key}
+								>
+									<Switch location={location}>
+										<Route exact path={routes.WELCOME_EXPLORE} component={ExplorePage}/>
+										<Route exact path={routes.WELCOME_COLLECT} component={CollectPage}/>
+										<Route exact path={routes.WELCOME_JOIN} component={JoinPage}/>
+									</Switch>
+								</CSSTransition>
+							</TransitionGroup>
+						);
+					}}
+				/>
+				<Route exact path={routes.WELCOME} component={MapPage}/>
+				<Route exact path={routes.WELCOME_PROFILE} component={ProfilePage}/>
+				<Route exact path={routes.WELCOME_LIST} component={ListPage}/>
+			</Router>
+		);
+	}
+}
